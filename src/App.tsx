5d643d804dffff50a675ed87ef9b82c83d1a9752
root <root@DESKTOP-JNRM7PR>
2022-12-26 21:48:18 +0800

    feat:搭建框架

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import A from './lib';
+
+function App() {
+  return (
+    <div>
+      <A></A>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.jsx'),
+      name: 'React Library Vite',
+      fileName: (format) => `react-library-vite.${format}.js`
+    },
+    rollupOptions: {
+      // externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
+  plugins: [react()]
+})
